9ad840f2044b2b96ed52bb6db63b77e51f665d42
GSmith-UA <griffinlsmith@arizona.edu>
2025-11-10 11:08:48 -0700

    adding linear term? Fingers crossed

--- src/unpack_coeffs.m
+++ src/unpack_coeffs.m
@@ -1,6 +1,7 @@
 function coef = unpack_coeffs(beta, N, K)
     coef.c = beta(1);
-    coef.a = beta(2:1+N);
-    coef.alpha = beta(1+N+1 : 1+N+K);
-    coef.beta  = beta(1+N+K+1 : 1+N+2*K);
+    coef.d = beta(2); %GS Change
+    coef.a = beta(3:2+N); %GS Change
+    coef.alpha = beta(2+N+1 : 2+N+K); %GS Change
+    coef.beta  = beta(2+N+K+1 : 2+N+2*K); %GS Change
 end
